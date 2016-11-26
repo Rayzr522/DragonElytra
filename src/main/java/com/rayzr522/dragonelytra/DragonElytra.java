0f5d72291c8103135a8222b9f73b965db9fe036f
Rayzr522 <rayzr522@gmail.com>
2016-11-26 12:55:34 -0500

    Project 'DragonElytra' generated
    Generated using PerceivePluginArchetype and custom CLI project generator tool by Rayzr

---
+++ src/main/java/com/rayzr522/dragonelytra/DragonElytra.java
@@ -0,0 +1,12 @@
+package com.rayzr522.dragonelytra;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DragonElytra extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+        
+    }
+    
+}
