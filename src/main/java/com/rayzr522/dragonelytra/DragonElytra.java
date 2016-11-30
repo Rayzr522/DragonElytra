97433894a83c396ae2bdd395a3ae6b16368c1d04
Rayzr522 <rayzr522@gmail.com>
2016-11-30 15:47:33 -0500

    First commit!

--- src/main/java/com/rayzr522/dragonelytra/DragonElytra.java
+++ src/main/java/com/rayzr522/dragonelytra/DragonElytra.java
@@ -1,12 +1,25 @@
 package com.rayzr522.dragonelytra;
 
+import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class DragonElytra extends JavaPlugin {
-    
+public class DragonElytra extends JavaPlugin implements Listener {
+
     @Override
     public void onEnable() {
-        
+        getServer().getPluginManager().registerEvents(this, this);
     }
     
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent e) {
+        if (e.getEntityType() == EntityType.ENDER_DRAGON) {
+            e.getDrops().add(new ItemStack(Material.ELYTRA));
+        }
+    }
+
 }
